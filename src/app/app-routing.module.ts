045609c755a3837c44d3106a5307c76a2ae77abb
chelsac <chelsacleetuscc@gmail.com>
2022-10-25 10:54:13 +0530

    new

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+
+const routes: Routes = [
+                        {path:'',component:LandingSectionComponent},
+                        {path:'about',component:AboutComponent},
+                        {path:'services',component:ServicesSectionComponent},
+                        {path:'contactus',component:ContactUsComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
